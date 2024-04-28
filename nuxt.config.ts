fc29a821ca4a5de7ca497e32c676f6a3e6c71ea3
Miguel Domingos <mighueldomingos@gmail.com>
2024-04-28 17:12:17 +0200

    try github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   router: {
+    base: "metaverse",
     options: {
       linkActiveClass: "active",
       linkExactActiveClass: "exact-active",
